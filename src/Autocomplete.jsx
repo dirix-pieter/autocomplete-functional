0ccb7ab6fb62200c41d764d33c9949e261de3b85
Pieter Dirix <pieter.dirix@easytocare.be>
2022-12-19 09:51:26 +0100

    init functional js widget

---
+++ src/Autocomplete.jsx
@@ -0,0 +1,12 @@
+import { createElement } from "react";
+
+import { HelloWorldSample } from "./components/HelloWorldSample";
+import "./ui/Autocomplete.css";
+
+export default Autocomplete = ({ sampleText }) => {
+    return (
+        <div>
+            <input type="text"/>
+        </div>
+    );
+}
